f43738884cf9814b8720edc61bf250eaf705c6d0
Ilya Besedin <125263645+IlyaBesedin@users.noreply.github.com>
2026-03-15 20:26:13 +0100

    List passed and failed tests

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : [['list'], ['html', { open: 'never' }]],
+  reporter: process.env.CI ? [['github'], ['list'], ['html', { open: 'never' }]] : [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://127.0.0.1:3000',
     testIdAttribute: 'data-testid',
